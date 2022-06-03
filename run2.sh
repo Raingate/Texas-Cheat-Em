2610437bb2213005375176b22d3621c6aedc0d09
❤Eula❤ <66993346+darkcorvushhh@users.noreply.github.com>
2022-06-03 17:13:35 +0800

    Delete run2.sh

--- run2.sh
+++
@@ -1,6 +0,0 @@
-python train.py --player1 cheater --player2 ppo
-python train.py --player1 cheater --player2 dqn
-python train.py --player1 cheater --player2 raise_rule
-python train.py --player1 cheater --player2 rule
-python train.py --player1 cheater --player2 random
-python train.py --player1 cheater --player2 table
