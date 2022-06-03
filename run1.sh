8b5545c787baf4628eb43b5e64e85d74d2f0506f
❤Eula❤ <66993346+darkcorvushhh@users.noreply.github.com>
2022-06-03 17:13:26 +0800

    Delete run1.sh

--- run1.sh
+++
@@ -1,6 +0,0 @@
-python train.py --player1 table --player2 random
-python train.py --player1 table --player2 rule
-python train.py --player1 table --player2 raise_rule
-python train.py --player1 random --player2 rule
-python train.py --player1 random --player2 raise_rule
-python train.py --player1 rule --player2 raise_rule
