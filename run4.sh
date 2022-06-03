4d9446c76a7bd7d6d4e2a222e9ba9dd5ae8071e6
❤Eula❤ <66993346+darkcorvushhh@users.noreply.github.com>
2022-06-03 17:14:33 +0800

    Delete run4.sh

--- run4.sh
+++
@@ -1,4 +0,0 @@
-python train.py --player1 dqn --player2 raise_rule
-python train.py --player1 dqn --player2 rule
-python train.py --player1 dqn --player2 random
-python train.py --player1 dqn --player2 table
